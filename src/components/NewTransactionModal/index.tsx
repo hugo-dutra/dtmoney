ec3d8ce69dfb52ebf30693f48fa538ae8043d458
hugo-dutra <hugo.dutra@hotmail.com>
2022-04-07 20:34:08 -0300

    post transactions finished

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,16 +1,31 @@
-import React, { useState } from 'react'
-import Modal from 'react-modal'
+import React, { FormEvent, useState } from 'react'
+import Modal from 'react-modal';
 import closeImg from '../../assets/Botão - Fechar.svg';
-import { Container } from './styles';
+import { Container, TransactionTypeContainer, RadioBox } from './styles';
+import incomeImg from '../../assets/Entradas.svg';
+import outcomeImg from '../../assets/Saídas.svg';
+import { api } from '../../services/api';
 
 
 Modal.setAppElement('#root');
 
-
-
 export const NewTransactionModal: React.FC<{ isOpen: boolean, onRequestClose: () => void }> = (props) => {
+  const [type, setType] = useState('deposit');
+  const [title, setTitle] = useState('');
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState('');
+
+  const handleCreateNewTransaction = (e: FormEvent) => {
+    e.preventDefault();
+    const data = {
+      type, title, value, category
+    }
+    api.post('/transactions', data)
+
+  }
+
   return (
-    <Container>
+    <Container onSubmit={handleCreateNewTransaction}>
       <Modal
         isOpen={props.isOpen}
         onRequestClose={props.onRequestClose}
@@ -25,9 +40,30 @@ export const NewTransactionModal: React.FC<{ isOpen: boolean, onRequestClose: ()
         </button>
         <Container>
           <h2>Cadastrar transação</h2>
-          <input placeholder='Título' />
-          <input placeholder='Valor' type="number" />
-          <input placeholder='Categoria' />
+          <input placeholder='Título' onChange={e => setTitle(title => e.target.value)} value={title} />
+          <input placeholder='Valor' type="number" onChange={e => setValue(value => Number(e.target.value))} value={value} />
+          <TransactionTypeContainer>
+            <RadioBox
+              type="button"
+              onClick={() => { setType('deposit') }}
+              isActive={type === 'deposit'}
+              activeColor="green"
+            >
+              <img src={incomeImg} alt="Entrada" />
+              Entrada
+            </RadioBox>
+            <RadioBox
+              type="button"
+              onClick={() => { setType('withdraw') }}
+              isActive={type === 'withdraw'}
+              activeColor="red"
+            >
+              <img src={outcomeImg} alt="Saida" />
+              Saída
+            </RadioBox>
+          </TransactionTypeContainer>
+
+          <input placeholder='Categoria' onChange={e => setCategory(category => e.target.value)} value={category} />
           <button type='submit'>Cadastrar</button>
         </Container>
       </Modal>
--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,21 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 
 createServer({
+
+  models: {
+    transaction: Model
+  },
+
   routes() {
     this.namespace = 'api';
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createAt: new Date()
-        },
-        {
-          id: 2,
-          title: 'Transaction 2',
-          amount: 450,
-          type: 'deposit',
-          category: 'Transport',
-          createAt: new Date()
-        }
-      ]
+      return this.schema.all('transaction')
+    })
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+      return schema.create('transaction', data);
     })
   }
 })
