9b1c5fc7e51def5347dcc1fd6da9b0168c95e35a
hugo-dutra <hugo.dutra@hotmail.com>
2022-04-08 08:39:58 -0300

    feat: Table finished

--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -1,12 +1,21 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { api } from "../../services/api"
 import { Container } from "./styles"
 
+type Transaction = {
+    id: number;
+    title: string;
+    amount: number;
+    type: string;
+    category: string;
+    createdAt: string;
+}
 
 export const TransactionsTable = () => {
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
     useEffect(() => {
         api('transactions')
-            .then(response => console.log(response.data))
+            .then(response => setTransactions(response.data.transactions))
     }, [])
 
     return (
@@ -21,24 +30,12 @@ export const TransactionsTable = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Desenvolvimento website</td>
-                        <td className="deposit">12000</td>
-                        <td>Desenvolvimento</td>
-                        <td>20/02/2021</td>
-                    </tr>
-                    <tr>
-                        <td>Desenvolvimento website</td>
-                        <td className="withdraw">12000</td>
-                        <td>Desenvolvimento</td>
-                        <td>17/02/2021</td>
-                    </tr>
-                    <tr>
-                        <td>Desenvolvimento website</td>
-                        <td className="deposit">12000</td>
-                        <td>Desenvolvimento</td>
-                        <td>20/02/2021</td>
-                    </tr>
+                    {transactions.map(tran => (<tr key={tran.id}>
+                        <td>{tran.title}</td>
+                        <td className={tran.type}> {new Intl.NumberFormat('pt-Br', { style: 'currency', currency: 'BRL' }).format(tran.amount)}</td>
+                        <td>{tran.category}</td>
+                        <td className={tran.type}> {new Intl.DateTimeFormat('pt-Br').format(new Date(tran.createdAt))}</td>
+                    </tr>))}
                 </tbody>
             </table>
         </Container>
